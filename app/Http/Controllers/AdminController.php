e5ff32da05a198fb8f932f40cb78178630bbd0bf
Vickyleonardoo <vickyleonardo23@gmail.com>
2023-10-26 16:13:04 +0800

    fix count

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -22,7 +22,7 @@ class AdminController extends Controller
 
         return view('admin.viewHome',[
             'title' => 'Penkopi - Home',
-            'karyawan' => User::where('role' , 3)->count(),
+            'karyawan' => User::where('role' , 3)->where('is_active','0')->count(),
             'outlet' => Outlet::count(),
             'izin' => Izin::where('status','Pending')->count(),
             'data_absen' => $data_absen->get(),
